d9d6ed3bf66fca5a7b2cda208675dbd96aa205fd
Parth0415 <parthmali1597@gmail.com>
2023-03-17 15:10:24 -0400

    Added questions for roles, employee and department

--- server.js
+++ server.js
@@ -38,12 +38,16 @@ const questionsForMenu = [
   },
 ];
 
+
+const myDepartments= [];
+
+const roles= [];
+
+const employees= [];
+
 const questionForAddDepartment = [
     { name: "name", message: "What is the name of the department?", type: "input" },
 ];
-
-const myDepartments= []
-
 
 const questionForAddRole = [
     { name: "name", message: "What is the name of the role?", type: "input" },
@@ -51,6 +55,77 @@ const questionForAddRole = [
     { name: "name", message: "Which department does the role belong to?", type: "list", choices: myDepartments}
 ];
 
+const questionForAddEmployees = [
+    { name: "name", message: "What is the employee's first name?", type: "input" },
+    { name: "name", message: "What is the employee's last name?", type: "input" },
+    { name: "name", message: "What is the employee's role?", type: "list", choices: roles},
+    { name: "name", message: "Who is the employee's manager?", type: "list", choices: employees}
+]
+
+async function init(){
+    const menuAnswer = await inquirer.prompt(questionsForMenu)
+    console.log(menuAnswer)
+    switch(menuAnswer.option){
+        case "View all employees":
+            viewAllEmployees();
+            console.log("lets view all of employees")
+            break;
+        case "Add employees":
+            console.log("adding employees")
+            break;
+        case "Update employee role":
+            console.log("Role of employees updated")
+            break;
+        case "View all roles":
+            viewAllRoles();
+            console.log("lets view all of roles")
+            break;
+        case "Add roles":
+            console.log("role added")
+            break;
+        case "View all departments":
+            viewAllDepartment();
+            console.log("lets view all of departhments")
+            break;
+        case "Add department":
+            console.log("department added")
+            break;
+        case "Quit":
+            console.log("exit")
+            break;
+    }
+
+
+
+}
+
+function viewAllEmployees(){
+    db.query("SELECT * FROM employee", (err, results)=>{
+        console.table(results)
+    })
+};
+
+function viewAllRoles(){
+    db.query("SELECT * FROM role", (err, results)=>{
+        console.table(results)
+    })
+};
+
+function viewAllDepartment(){
+    db.query("SELECT * FROM department", (err, results)=>{
+        console.table(results)
+    })
+};
+
+
+
+
+
+
+
 app.use((req, res) => {
   res.status(404).end();
 });
+
+
+init()

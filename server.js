fcc0eaf71326abdbd43b88bdf9ee19e96d0d09d9
Parth0415 <parthmali1597@gmail.com>
2023-03-21 20:32:47 -0400

    Completed view all Employees roles with specific data

--- server.js
+++ server.js
@@ -33,137 +33,199 @@ const questionsForMenu = [
       "Add role",
       "View all departments",
       "Add department",
-      "Quit"
+      "Quit",
     ],
   },
 ];
 
+const myDepartments = [];
 
-const myDepartments= [];
+const roles = [];
 
-const roles= [];
-
-const employees= [];
+const employees = [];
 
 const questionForAddDepartment = [
-    { name: "name", message: "What is the name of the department?", type: "input" },
+  {
+    name: "name",
+    message: "What is the name of the department?",
+    type: "input",
+  },
 ];
 
 const questionForAddRole = [
-    { name: "name", message: "What is the name of the role?", type: "input" },
-    { name: "salary", message: "What is the salary of the role?", type: "input" },
-    { name: "department_id", message: "Which department does the role belong to?", type: "list", choices: myDepartments}
+  { name: "name", message: "What is the name of the role?", type: "input" },
+  { name: "salary", message: "What is the salary of the role?", type: "input" },
+  {
+    name: "department_id",
+    message: "Which department does the role belong to?",
+    type: "list",
+    choices: myDepartments,
+  },
 ];
 
 const questionForAddEmployees = [
-    { name: "first_name", message: "What is the employee's first name?", type: "input" },
-    { name: "last_name", message: "What is the employee's last name?", type: "input" },
-    { name: "role_id", message: "What is the employee's role?", type: "list", choices: roles},
-    { name: "manager_id", message: "Who is the employee's manager?", type: "list", choices: employees}
-]
+  {
+    name: "first_name",
+    message: "What is the employee's first name?",
+    type: "input",
+  },
+  {
+    name: "last_name",
+    message: "What is the employee's last name?",
+    type: "input",
+  },
+  {
+    name: "role_id",
+    message: "What is the employee's role?",
+    type: "list",
+    choices: roles,
+  },
+  {
+    name: "manager_id",
+    message: "Who is the employee's manager?",
+    type: "list",
+    choices: employees,
+  },
+];
 
-async function init(){
-    db.query(`SELECT * FROM department`, (err, results)=>{
-        results.forEach((element)=>{
-            myDepartments.push({name: element.name, value: element.id})
-        })
-    })
+async function init() {
+  db.query(`SELECT * FROM department`, (err, results) => {
+    results.forEach((element) => {
+      myDepartments.push({ name: element.name, value: element.id });
+    });
+  });
 
-    db.query(`SELECT * FROM role`, (err,results)=>{
-        results.forEach((element)=>{
-            roles.push({name: element.title, value: element.id, salary: element.salary, department_id: element.department_id})
-        })
+  db.query(`SELECT * FROM role`, (err, results) => {
+    results.forEach((element) => {
+      roles.push({
+        name: element.title,
+        value: element.id,
+        salary: element.salary,
+        department_id: element.department_id,
+      });
+    });
     //    console.log(roles);
-    } )
+  });
 
-    db.query(`SELECT * FROM employee`, (err,results)=>{
-        results.forEach((element)=>{
-            employees.push({name: element.first_name +" "+element.last_name, value:element.id, role_id: element.role_id, manager_id:element.manager_id})
-            
-        })
-        // console.log(results);
-    })
-       
-   
+  db.query(`SELECT * FROM employee`, (err, results) => {
+    results.forEach((element) => {
+      employees.push({
+        name: element.first_name + " " + element.last_name,
+        value: element.id,
+        role_id: element.role_id,
+        manager_id: element.manager_id,
+      });
+    });
+    // console.log(results);
+  });
 
-    const menuAnswer = await inquirer.prompt(questionsForMenu)
-    switch(menuAnswer.option){
-        case "View all employees":
-            viewAllEmployees();
-            console.log("lets view all of employees")
-            break;
-        case "Add employees":
-           await addEmployee();
-            break;
-        case "Update employee role":
-            console.log("Role of employees updated")
-            break;
-        case "View all roles":
-            viewAllRoles();
-            console.log("lets view all of roles")
-            break;
-        case "Add role":
-            await addRole();
-            break;
-        case "View all departments":
-            viewAllDepartment();
-            console.log("lets view all of departhments")
-            break;
-        case "Add department":
-            await addDepartment();
-            break;
-        case "Quit":
-            console.log("exit")
-            break;
-    }
-
-
-
+  const menuAnswer = await inquirer.prompt(questionsForMenu);
+  switch (menuAnswer.option) {
+    case "View all employees":
+      viewAllEmployees();
+      console.log("lets view all of employees");
+      break;
+    case "Add employees":
+      await addEmployee();
+      break;
+    case "Update employee role":
+      console.log("Role of employees updated");
+      break;
+    case "View all roles":
+      viewAllRoles();
+      console.log("lets view all of roles");
+      break;
+    case "Add role":
+      await addRole();
+      break;
+    case "View all departments":
+      viewAllDepartment();
+      console.log("lets view all of departhments");
+      break;
+    case "Add department":
+      await addDepartment();
+      break;
+    case "Quit":
+      console.log("exit");
+      break;
+  }
 }
 
-function viewAllEmployees(){
-    db.query("SELECT * FROM employee", (err, results)=>{
-        console.table(results)
-    })
-};
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", (err, results) => {
+    results.forEach((element) => {
+      var employeesRole = roles.filter((role) => {
+        return role.value == element.role_id;
+      });
+      element.title = employeesRole[0].name;
+      element.salary = employeesRole[0].salary;
 
-function viewAllRoles(){
-    db.query("SELECT * FROM role", (err, results)=>{
-      //  console.log(results);
-       
-        console.table(results)
-    })
-};
+      var employeeDepartment = myDepartments.filter((department) => {
+        return department.value == employeesRole[0].department_id;
+      });
 
-function viewAllDepartment(){
-    db.query("SELECT * FROM department", (err, results)=>{
-        console.table(results)
-    })
-};
+      element.department = employeeDepartment[0].name;
+      delete element.role_id;
 
-async function addDepartment(){
-    const newDepartmentData = await inquirer.prompt(questionForAddDepartment)
-    console.log(newDepartmentData)
-    db.query(`INSERT INTO department (name) VALUES ('${newDepartmentData.name}')`);
+      // element.manager_id= employeesRole[0].manager_id
+
+      var managerData = employees.filter((employee) => {
+        return employee.value == element.manager_id;
+      });
+      //console.log(managerData)
+      element.manager =
+        managerData[0] == undefined ? "null" : managerData[0].name;
+      delete element.manager_id;
+    });
+    console.table(results);
+  });
 }
 
-async function addRole(){
-    const newRoleData = await inquirer.prompt(questionForAddRole)
-    console.log(newRoleData)
-   db.query(`INSERT INTO role (title,salary,department_id) VALUES ('${newRoleData.name}','${newRoleData.salary}','${newRoleData.department_id}')`);
+function viewAllRoles() {
+  db.query("SELECT * FROM role", (err, results) => {
+    results.forEach((element) => {
+      var departmentOfRole = myDepartments.filter((department) => {
+        return department.value == element.department_id;
+      });
+      element.department = departmentOfRole[0].name;
+      delete element.department_id;
+    });
+    console.table(results);
+  });
 }
 
-async function addEmployee(){
-    const newEmployeeData = await inquirer.prompt(questionForAddEmployees)
-    console.log(newEmployeeData)
-     db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${newEmployeeData.first_name}','${newEmployeeData.last_name}','${newEmployeeData.role_id}','${newEmployeeData.manager_id}')`);
+function viewAllDepartment() {
+  db.query("SELECT * FROM department", (err, results) => {
+    console.table(results);
+  });
 }
 
+async function addDepartment() {
+  const newDepartmentData = await inquirer.prompt(questionForAddDepartment);
+  console.log(newDepartmentData);
+  db.query(
+    `INSERT INTO department (name) VALUES ('${newDepartmentData.name}')`
+  );
+}
 
+async function addRole() {
+  const newRoleData = await inquirer.prompt(questionForAddRole);
+  console.log(newRoleData);
+  db.query(
+    `INSERT INTO role (title,salary,department_id) VALUES ('${newRoleData.name}','${newRoleData.salary}','${newRoleData.department_id}')`
+  );
+}
+
+async function addEmployee() {
+  const newEmployeeData = await inquirer.prompt(questionForAddEmployees);
+  console.log(newEmployeeData);
+  db.query(
+    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${newEmployeeData.first_name}','${newEmployeeData.last_name}','${newEmployeeData.role_id}','${newEmployeeData.manager_id}')`
+  );
+}
 
 app.use((req, res) => {
   res.status(404).end();
 });
 
-
-init()
+init();
